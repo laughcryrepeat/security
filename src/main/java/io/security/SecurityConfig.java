19e7795b1c7ae1e004caf6341a007db288341ec3
오현화 <meke@nate.com>
2021-12-12 20:38:46 +0900

    Add rememberMe
    - 리멤버미 추가

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -5,12 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -19,6 +21,9 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserDetailsService userDerailsService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -72,5 +77,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           }
         })
         .deleteCookies("remember-me");
+    http
+        .rememberMe()
+        .rememberMeParameter("remember")  // 기본 파라미터명은 remember-me
+        .tokenValiditySeconds(3600) // Default는 14일
+        //.alwaysRemember(true) // 리멤버미 기능이 활성화되지 않아도 항상 실행. 대체로 설정x
+        .userDetailsService(userDerailsService);
   }
 }
